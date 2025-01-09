9e68c687d52efd976306dc12851db5f5d3d53906
Deiby Deaquiz <alxds.ad@gmail.com>
2025-01-09 16:55:39 -0500

    feat: se hace la conexcion a la base de datos y se generan los recursos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EstudianteModule } from './estudiante/estudiante.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PruebaModule } from './prueba/prueba.module';
+import { PreguntaModule } from './pregunta/pregunta.module';
+import { ResultadoModule } from './resultado/resultado.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('HOST_BBDD'),
+        port: Number(configService.get('PORT_BBDD')),
+        username: configService.get('USER_BBDD'),
+        password: configService.get('PASSWRD_BBDD'),
+        database: configService.get('BBDD'),
+        entities: [],
+        synchronize: false,
+        autoLoadEntities:true,
+        subscribers:[
+        ],
+      }),
+      inject:[ConfigService]     
+    }),
+    EstudianteModule,
+    PruebaModule,
+    PreguntaModule,
+    ResultadoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+  const puerto = configService.get('PORT_APP') ? configService.get('PORT_APP') : 3000;
+  await app.listen(
+    puerto || 3000 , () => {
+      console.log("******************\nServer run in port\n==>    " + puerto+"    <==\n******************");
+    }
+  );
+
 }
 bootstrap();
