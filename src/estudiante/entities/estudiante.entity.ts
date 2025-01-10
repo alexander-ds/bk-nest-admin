a86b4509adb25c5a5fe5aa6782c02304f92873fd
Deiby Deaquiz <alxds.ad@gmail.com>
2025-01-09 20:25:35 -0500

    feat: se agregan las estructuras de bases de datos

--- src/estudiante/entities/estudiante.entity.ts
+++ src/estudiante/entities/estudiante.entity.ts
@@ -1,1 +1,21 @@
-export class Estudiante {}
+import { Resultado } from 'src/resultado/entities/resultado.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('Estudiante')
+export class Estudiante  {
+    @PrimaryGeneratedColumn()
+    ID:string;
+
+    @Column()
+    Nombres:string;
+
+    @Column()
+    Grado:string;
+
+    @Column()
+    Salon:string;
+
+    @OneToMany(()=>Resultado,(resultado)=>resultado.IDEstudiante)
+    resultados:Resultado[];
+
+}
